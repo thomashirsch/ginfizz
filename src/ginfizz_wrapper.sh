89e8efd93d75fa3c5a2d188e9c6e4480be1e2eb1
Hirsch <thomas.antoine.hirsch.gumovski@gmail.com>
2017-03-22 17:02:37 +0100

    plus wrapper plus dataset

---
+++ src/ginfizz_wrapper.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# env var for dev machine
+MCRROOT=/homes_unix/hirsch/historique_fli_iam/essai_spm_stand_alone/mcr2016/v91
+SPMSAROOT=/homes_unix/hirsch/historique_fli_iam/essai_spm_stand_alone/spm12/run_spm12.sh
+CODEROOT=/homes_unix/hirsch/ginfizz/src
+DATAROOT=/scratch/user/hirsch/datadirp
+
+# env var for docker machines, for VIP
+# MCRROOT=/opt/mcrbis/v90
+# MCRROOTBIS=/opt/mcr/v91
+# SPMSAROOT=/opt/spm/spm12
+# CODEROOT=/rstp_code
+# DATAROOT=/rstp_data
+
+info "Start running ginfizz wrapper"
+
+function die {
+    local D=`date`
+    echo "[ $D ] ERROR: $*"
+    exit 1
+}
+
+function info {
+    local D=`date`
+    echo "[ $D ] INFO: $*"
+}
+
+if [ $# != 2 ]
+then
+    die "usage: $0 <input_tgz>  <output_dir>"
+fi
+
+INPUTFILE=$1
+OUTPUTDIR=$2
+info "parameters are ${INPUTFILE} and ${OUTPUTDIR} "
+# it is lacking the path of the tar command
+
+info "PATH is ${PATH}"
+info "INPUTFILE is ${INPUTFILE}"
+
+cd ${DATAROOT}
+
+pwd
+
+# untar of the inputfile 
+TOP=`tar --exclude '*/*' --overwrite -tzf ${INPUTFILE}` || die "Cannot get top level directory from ${INPUTFILE}!"
+info "TOP is ${TOP}"
+tar zxf ${INPUTFILE} || die "Cannot untargz ${INPUTFILE}!"
+
+info "TOP is ${TOP}"
+export TOP
+
+# List top directory in subject directory. There must be only 1. 
+NSECONDTOP=`ls ${DATAROOT}/${TOP} | wc -l| awk '{print $1}'` || die "Cannot count directories in ${DATAROOT}/${TOP}!"
+if [ ${NSECONDTOP} -ne 1 ] 
+then
+    die "Found 0 or more than 1 directory in ${PWD}!"
+fi
+
+SECONDTOP=`ls ${DATAROOT}/${TOP} || die "Cannot find directory in ${DATAROOT}/${TOP}"`
+info "SECONDTOP is ${SECONDTOP}"
+
+TROISTOP=`ls ${DATAROOT}/${TOP}/${SECONDTOP} || die "Cannot find directory in ${DATAROOT}/${TOP}/${SECONDTOP}"`
+info "TROISTOP is ${TROISTOP}"
+
+
+# Export FLI base directory expected by the matlab script. 
+export FLIBASEDIR=${DATAROOT}/${TOP}/${SECONDTOP}/${TROISTOP}
+info "FLIBASEDIR is ${FLIBASEDIR}"
+
+# Export ATLAS base directory expected by the matlab script. 
+export ATLASBASEDIR=${DATAROOT}/${TOP}/${SECONDTOP}/${TROISTOP}/Atlases
+
+info "ATLASBASEDIR is ${ATLASBASEDIR}"
+
+
+
+# find the XML file
+# Search for the XML file of the subject
+NXMLFILES=`ls ${FLIBASEDIR}/*.xml | wc -l | awk '{print $1}'` || die "Cannot count xml files in ${FLIBASEDIR}!"
+if [ ${NXMLFILES} -ne 1 ] 
+then
+    die "Found 0 or more than 1 xml file in ${FLIBASEDIR}!"
+fi
+XMLFILE=`ls ${FLIBASEDIR}/*.xml` || die "Cannot find xml file in ${FLIBASEDIR}!"
+info "XMLFILE is ${XMLFILE}"
+
+# find the Atlas file
+# Search for the .nii file of the subject
+ATLASFILE=`ls ${ATLASBASEDIR}/*.nii | wc -l | awk '{print $1}'` || die "Cannot count nii files in ${ATLASBASEDIR}!"
+if [ ${ATLASFILE} -ne 1 ] 
+then
+    die "Found 0 or more than 1 nii file in ${ATLASBASEDIR}!"
+fi
+ATLASFILE=`ls ${ATLASBASEDIR}/*.nii` || die "Cannot find nii file in ${ATLASBASEDIR}!"
+info "ATLASFILE is ${ATLASFILE}"
+
+
+
+
+
+
+
+# preparation of the results
+cd ${DATAROOT};
+mkdir results;
+RESULTSDIR=${DATAROOT}/results;
+info "RESULTSDIR is ${RESULTSDIR}"
+export RESULTSDIR;
+
+
+
+# 1 - make the python preprocess run
+(cd ${CODEROOT};
+pwd;
+exec   python ./ginfizz_preprocess.py  ${SPMSAROOT}   ${MCRROOT}  ${FLIBASEDIR}   ${ATLASFILE} ${RESULTSDIR} ;
+info "1 - python preprocess sent") &&
+
+
+
+# 4 - create a tarball from the results then give it to VIP by the outputdir argument
+( info "RESULTSDIR is ${RESULTSDIR}";
+  cd  ${RESULTSDIR}; 
+  pwd;
+  ls ;
+  # and copy the logs file to the outputdir
+  ls -l ${CODEROOT}/*.log
+  cp ${CODEROOT}/*.log ${OUTPUTDIR};
+  info  "we are sending the tar fct for the following repository"
+  
+  tar czf ${OUTPUTDIR}/data_results.tar.gz   ${RESULTSDIR} ;
+  info "4 eval has been sent we get the results in a tarball we give the tarball to VIP";)
+
+info "End running ginfizz wrapper"
